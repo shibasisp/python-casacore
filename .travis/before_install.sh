2e7729ae1243519dc2c4194e22d48cdb1f45fcb6
Shibasis Patel <smartshibasish@gmail.com>
2017-07-23 01:59:43 +0530

    modified according to the travis unsupported lang

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,25 +1,21 @@
 #!/usr/bin/env bash
 
 # Inspired by https://conda.io/docs/travis.html
-
 set -e
 set -v
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update; fi
-# if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
-# 	if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-#   		brew install python;
-#   		virtualenv venv -p python;
-#   		source venv/bin/activate;
-# 	else
-#   		brew install python3;
-#   		virtualenv venv -p python3;
-#   		source venv/bin/activate;
-#   	fi
-# fi
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew list python &>/dev/null || brew install python; fi
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew list python3 &>/dev/null || brew install python3; fi
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew install pyenv-virtualenv; fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
+	if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+  		brew install python;
+  		virtualenv venv -p python;
+  		source venv/bin/activate;
+	else
+  		brew install python3;
+  		virtualenv venv -p python3;
+  		source venv/bin/activate;
+  	fi
+fi
 
 
 if [ "$TRAVIS_OS_NAME" = linux ]; then
