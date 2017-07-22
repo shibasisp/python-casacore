087a6c22ff7a0b8aca9eb03aa0622a972a0b267e
Shibasis Patel <smartshibasish@gmail.com>
2017-07-22 14:55:09 +0530

    Added build matrix

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -4,17 +4,19 @@
 set -e
 set -v
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update          ; fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update; fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
 	if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
   		brew install python;
+  		virtualenv venv -p python;
+  		source venv/bin/activate;
 	else
   		brew install python3;
+  		virtualenv venv -p python3;
+  		source venv/bin/activate;
   	fi
 fi
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then virtualenv venv -p python; fi
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then source venv/bin/activate; fi
 
 if [ "$TRAVIS_OS_NAME" = linux ]; then
     sudo apt-get update
