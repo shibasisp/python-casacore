29c8822449017532ee2b1c01c2836ec8f0bb6941
Shibasis Patel <smartshibasish@gmail.com>
2017-07-23 15:34:35 +0530

    reverted back

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,24 +8,15 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     case "${TOXENV}" in
         py27)
             # Install some custom Python 2.7 requirements on OS X
-            brew update;
-            brew install python;
-            virtualenv venv -p python;
-            source venv/bin/activate;
+            
             ;;
         py35)
             # Install some custom Python 3.5 requirements on OS X
-            brew update;
-            brew install python;3
-            virtualenv venv -p python3;
-            source venv/bin/activate;
+            
             ;;
         py36)
             # Install some custom Python 3.6 requirements on OS X
-            brew update;
-            brew install python3;
-            virtualenv venv -p python3;
-            source venv/bin/activate;
+            
             ;;
     esac
 else
